45de192ecf3ca9503db92d90a0bcec7dfb0aedb3
Kazi Mehedi Hasan <rakibtg@gmail.com>
2019-04-16 11:04:46 +0600

    implemented sort handler

--- src/App.js
+++ src/App.js
@@ -6,17 +6,57 @@ import Search from './components/Search/Search'
 
 class App extends Component {
 
+  state = {
+    posts: [],
+    loading: true,
+  }
+
   componentDidMount() {
     document.title = 'Popular Products'
+    this.setState({
+      posts: PostData.sort((a, b) => b.votes_count - a.votes_count)
+    }, () => {
+      this.setState({
+        loading: false,
+      })
+    })
+    console.log(PostData)
+  }
+
+  handleSorting(type, sort) {
+    const { posts } = this.state
+    const sortKey = type === 'votes'
+      ? 'votes_count'
+      : 'comments_count'
+    const sorted = posts.sort(
+      (a, b) => sort 
+        ? a[sortKey] - b[sortKey]
+        : b[sortKey] - a[sortKey]
+    )
+    this.setState({
+      posts: sorted,
+      loading: true,
+    }, () => {
+      this.setState({
+        loading: false,
+      })
+    })
   }
 
   render() {
+    const { posts, loading } = this.state
     return (
       <div className="App">
         
         <Header />
-        <Search />
-        <PostLists posts={PostData} />
+        <Search 
+          sorting={ this.handleSorting.bind(this) }/>
+        
+        {
+          loading 
+            ? <div>Please wait</div>
+            : <PostLists posts={posts} />
+        }
         
       </div>
     )
