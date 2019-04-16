ea97a8c249df0236bf0fe621fe70d501d48f7223
Kazi Mehedi Hasan <rakibtg@gmail.com>
2019-04-16 10:12:22 +0600

    post thumbnail component

---
+++ src/components/PostThumbnail/PostThumbnail.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Pane, Button, Text, Heading } from 'evergreen-ui'
+import './PostThumbnail.css'
+
+class PostThumbnail extends Component {
+
+  state = {
+    post: null
+  }
+
+  componentDidMount() {
+    const { post } = this.props
+    this.setState({ post })
+  }
+
+  render() {
+    const { post } = this.state
+    if( post !== null ) {
+      return (
+        <Pane 
+          display="flex" 
+          padding={16}
+          justifyContent="center" 
+          borderRadius={0}
+          borderBottom="muted"
+          borderLeft="muted"
+          borderRight="muted">
+          <Pane className="app-header" display="flex" flexGrow={1}>
+            <Pane marginRight={20}>
+              <img className="product-image" src={post.thumbnail} />
+            </Pane>
+            <Pane flex={1} display="flex" flexDirection="column" justifyContent="center">
+              <Heading size={600}>{post.title}</Heading>
+              <Text>{post.snippet}</Text>
+            </Pane>
+            <Pane alignItems="center" display="flex">
+              <Button marginRight={12} iconBefore="symbol-triangle-up" appearance="minimal">{post.votes_count}</Button>
+              <Button marginRight={12} iconBefore="comment" appearance="minimal" intent="warning">{post.comments_count}</Button>
+            </Pane>
+          </Pane>
+        </Pane>
+      )
+    } else {
+      return <div></div>
+    }
+  }
+}
+
+export default PostThumbnail
