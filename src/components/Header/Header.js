5b2778c60fc8e2a1b7ec554cc87db3767e4320bf
Kazi Mehedi Hasan <rakibtg@gmail.com>
2019-07-10 11:30:34 +0600

    header app consumer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
 import './Header.css'
-import { Pane, Heading } from 'evergreen-ui'
+import { Pane, Heading, Button } from 'evergreen-ui'
+
+import { AppContext } from '../../contexts'
 
 class Header extends Component {
   
   render() {
-    return <Pane 
-      display="flex" 
-      padding={16} 
-      background="tint1" 
-      justifyContent="center" 
-      borderRadius={3}
-      borderBottom="muted">
-      <Pane className="app-header" display="flex" flexGrow={1}>
-        <Pane flex={1} alignItems="center" display="flex">
-          <Heading size={600}>Popular Products</Heading>
+    return <AppContext.Consumer>
+      { context => {
+        const { handleModal } = context
+        return <Pane 
+          display="flex" 
+          padding={16} 
+          background="tint1" 
+          justifyContent="center" 
+          borderRadius={3}
+          borderBottom="muted">
+          <Pane className="app-header" display="flex" flexGrow={1}>
+            <Pane flex={1} alignItems="center" display="flex">
+              <Heading size={600}>Popular Products</Heading>
+            </Pane>
+            <Button onClick={handleModal} appearance="primary">
+              Login
+            </Button>
+          </Pane>
         </Pane>
-      </Pane>
-    </Pane>
+      }}
+    </AppContext.Consumer>
   }
 }
 
