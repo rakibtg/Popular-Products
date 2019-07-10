576ba672fd17cab117557d70de156bb08e4e88bc
Kazi Mehedi Hasan <rakibtg@gmail.com>
2019-07-10 12:10:51 +0600

    login form

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,25 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Pane, Heading, Button, Dialog } from 'evergreen-ui';
+import { Pane, Heading, Button, Dialog, TextInput } from 'evergreen-ui';
 
 import { AppContext } from '../../contexts'
 
 export default class Modal extends React.Component {
+
+  state = {
+    username: '',
+    password: '',
+  }
+
   render() {
     return <AppContext.Consumer>
       { context => {
         const { showModal } = context.state
-        const { handleModal } = context
+        const { handleModal, handleLoggedIn } = context
         return showModal 
           ? ReactDOM.createPortal(
             <div>
               <Dialog
                 isShown={showModal}
-                title="Dialog title"
+                title="Login"
                 onCloseComplete={handleModal}
-                confirmLabel="Custom Label"
+                hasFooter={false}
               >
-                Dialog content
+                <TextInput
+                  onChange={e => this.setState({ username: e.target.value })}
+                  value={this.state.username}
+                  placeholder="Username"
+                  marginBottom={15}
+                />
+                <TextInput
+                  onChange={e => this.setState({ password: e.target.value })}
+                  value={this.state.password}
+                  placeholder="Password"
+                  marginBottom={15}
+                  type="password"
+                />
+                <Pane>
+                  <Button onClick={() => {
+                    handleModal()
+                    handleLoggedIn(this.state.username)
+                  }} appearance="primary">
+                    Login
+                  </Button>
+                </Pane>
               </Dialog>
               {this.props.children}
             </div>,
