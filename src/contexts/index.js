9fcb65b7855d12d23cf5c79ff7a3a49265c1d75b
Kazi Mehedi Hasan <rakibtg@gmail.com>
2019-07-10 12:11:04 +0600

    handle user auth status

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -5,16 +5,25 @@ export const AppContext = React.createContext()
 export default class AppProvider extends React.Component {
   state = {
     showModal: false,
+    isLoggedIn: false,
+    user: '',
   }
   handleModal () {
-    console.log('inside handle modal')
     this.setState({ showModal: !this.state.showModal })
+  }
+  handleLoggedIn (user) {
+    console.log('handleLoggedIn', user)
+    this.setState({ 
+      user,
+      isLoggedIn: !this.state.isLoggedIn,
+    })
   }
   render () {
     const { children } = this.props
     return <AppContext.Provider value={{
       state: this.state,
-      handleModal: () => this.handleModal()
+      handleModal: () => this.handleModal(),
+      handleLoggedIn: (user) => this.handleLoggedIn(user),
     }}>
       { children }
     </AppContext.Provider>
